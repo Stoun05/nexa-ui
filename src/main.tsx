b458249cc160e80ee8479f00b0b5446a7e9cd9dc
Stoun05 <isa1590ag@gmail.com>
2026-07-15 19:33:29 +0500

    Use Nexa UI virtual runtime modules

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
-import './styles.css';
-import App from './App';
+import 'virtual:nexa-styles.css';
+import App from 'virtual:nexa-app';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
